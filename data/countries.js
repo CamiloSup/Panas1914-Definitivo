dba6a9c692f551d1cfe0a5790094c996f97134ca
Camilo <113814659+CamiloSup@users.noreply.github.com>
2026-05-24 21:36:19 -0500

    Update countries.js

--- data/countries.js
+++ data/countries.js
@@ -1,1 +1,31 @@
+const countries = [
+  {
+    name: "Alemania",
+    money: 10,
+    oil: 5,
+    steel: 8,
+    food: 12,
+    infantry: 5,
+    tanks: 0
+  },
 
+  {
+    name: "Francia",
+    money: 9,
+    oil: 6,
+    steel: 7,
+    food: 10,
+    infantry: 4,
+    tanks: 0
+  },
+
+  {
+    name: "Rusia",
+    money: 12,
+    oil: 4,
+    steel: 6,
+    food: 15,
+    infantry: 7,
+    tanks: 0
+  }
+];
