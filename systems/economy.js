504ebfe9b4d1c0eca4222fbdf4dd299b36df141a
Camilo <113814659+CamiloSup@users.noreply.github.com>
2026-05-24 21:37:49 -0500

    Update economy.js

--- systems/economy.js
+++ systems/economy.js
@@ -1,1 +1,9 @@
 
+function updateResources(country) {
+
+  document.getElementById("money").textContent = country.money;
+  document.getElementById("oil").textContent = country.oil;
+  document.getElementById("steel").textContent = country.steel;
+  document.getElementById("food").textContent = country.food;
+
+}
