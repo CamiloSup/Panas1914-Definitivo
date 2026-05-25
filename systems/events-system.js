3724f03dce001c488ed75dc43473a624558311ff
Camilo <113814659+CamiloSup@users.noreply.github.com>
2026-05-24 21:38:15 -0500

    Update events-system.js

--- systems/events-system.js
+++ systems/events-system.js
@@ -1,1 +1,10 @@
+function randomEvent() {
 
+  const randomIndex = Math.floor(Math.random() * events.length);
+
+  const event = events[randomIndex];
+
+  addNews(`EVENTO: ${event.title} - ${event.description}`);
+
+  return event;
+}
