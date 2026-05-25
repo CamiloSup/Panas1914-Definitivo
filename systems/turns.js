f3ccf374a67010fda895621caad830fdc233361f
Camilo <113814659+CamiloSup@users.noreply.github.com>
2026-05-24 21:38:48 -0500

    Update turns.js

--- systems/turns.js
+++ systems/turns.js
@@ -1,1 +1,25 @@
 
+let currentTurn = 1;
+let currentYear = 1914;
+
+function nextTurn() {
+
+  currentTurn++;
+
+  if (currentTurn % 4 === 0) {
+    currentYear++;
+  }
+
+  document.getElementById(
+    "turn-info"
+  ).textContent = `Turno ${currentTurn} - Año ${currentYear}`;
+
+  selectedCountry.money += 2;
+  selectedCountry.food += 1;
+  selectedCountry.oil += 1;
+  selectedCountry.steel += 1;
+
+  updateResources(selectedCountry);
+
+  addNews(`${selectedCountry.name} recibió recursos del nuevo turno.`);
+}
