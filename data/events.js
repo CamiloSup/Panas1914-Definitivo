4ec1fb17e4c28f9f5a848ad1d6d0aeceb6c33a28
Camilo <113814659+CamiloSup@users.noreply.github.com>
2026-05-24 21:37:05 -0500

    Update events.js

--- data/events.js
+++ data/events.js
@@ -1,1 +1,23 @@
+const events = [
 
+  {
+    title: "Telegrama interceptado",
+    description: "Puedes ver las cartas de otro jugador."
+  },
+
+  {
+    title: "Crisis económica",
+    description: "Pierdes 3 de dinero."
+  },
+
+  {
+    title: "Reclutamiento masivo",
+    description: "+3 de infantería."
+  },
+
+  {
+    title: "Sabotaje industrial",
+    description: "Pierdes 2 de acero."
+  }
+
+];
